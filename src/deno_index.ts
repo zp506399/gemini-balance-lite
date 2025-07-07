4548fa2ddb7a788feff6a62cbd30482e680e770e
tech-shrimp <techshrimp@163.com>
2025-07-07 11:39:28 +0800

    1

--- src/deno_index.ts
+++ src/deno_index.ts
@@ -1,9 +1,9 @@
 import { handleRequest } from "./handle_request.js";
 
-async function handleRequest(req: Request): Promise<Response> {
+async function denoHandleRequest(req: Request): Promise<Response> {
   const url = new URL(req.url);
   console.log('Request URL:', req.url);
   return handleRequest(req);
 };
 
-Deno.serve({ port: 80 },handleRequest); 
+Deno.serve({ port: 80 },denoHandleRequest); 
